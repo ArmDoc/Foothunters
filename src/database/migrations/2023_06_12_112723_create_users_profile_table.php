f7c5f6be3e99cc6113dd07bbfd72dac8d7864ed0
Arman Zarmanyan <zarman32@yandex.ru>
2023-06-13 15:01:48 +0400

    todo

--- src/database/migrations/2023_06_12_112723_create_users_profile_table.php
+++ src/database/migrations/2023_06_12_112723_create_users_profile_table.php
@@ -13,14 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('users_profile', function (Blueprint $table) {
             $table->id();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->int('user_id')->unique();
+            $table->string('type');
+            $table->string('name');
+            $table->string('surname');
+            $table->dateTime('birth_date');
+            $table->int('phone')->nullable();
             $table->timestamps();
-            $table->timestamp('deleted_at')->nullable();
         });
     }
 
@@ -31,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('users_profile');
     }
 };
